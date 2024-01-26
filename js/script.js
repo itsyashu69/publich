a15072988bf4544c38aa93d4b9bbff9c8d851476
itsyashu69 <148975754+itsyashu69@users.noreply.github.com>
2024-01-26 17:13:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 7, 2023 00:00:00").getTime();
+var countDownDate = new Date("Feb 17, 2024 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
