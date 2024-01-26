7b06b9a5ccef3bd4b2b7cfe5439d87054d5269c6
itsyashu69 <148975754+itsyashu69@users.noreply.github.com>
2024-01-26 11:53:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@
   When the website is backed up with a soul-stirring track, the feeling becomes absolutely surreal. 
   Choose a heart-touching track! 🎵 ❤️
  
-  Listen here: https://youtu.be/X0MDALpV29s
+  Listen here: https:youtu.be/X0MDALpV29s
  
  
 $(document).on('click', function(){
@@ -25,35 +25,35 @@ $(document).on('click', function(){
     console.log('Shaadi me zaroor aana');
 });
 
-// Set the date we're counting down to
+ Set the date we're counting down to
 var countDownDate = new Date("Feb 17, 2024 00:00:00").getTime();
 
-// Update the count down every 1 second
+ Update the count down every 1 second
 var x = setInterval(function() {
 
-    // Get todays date and time
+     Get todays date and time
     var now = new Date().getTime();
     
-    // Find the distance between now and the count down date
+     Find the distance between now and the count down date
     var distance = countDownDate - now;
     
-    // Time calculations for days, hours, minutes and seconds
+     Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000  60  60  24));
     var hours = Math.floor((distance % (1000  60  60  24)) / (1000  60  60));
     var minutes = Math.floor((distance % (1000  60  60)) / (1000  60));
     var seconds = Math.floor((distance % (1000  60)) / 1000);
     
-    // Output the result in an element with id="demo"
+     Output the result in an element with id="demo"
     document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
     
-    // If the count down is over, write some text 
+     If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
     }
 }, 1000);
 
-// being a bit cool :p  
+ being a bit cool :p  
 var styles = [
     'background: linear-gradient(#D33106, #571402)'
     , 'border: 4px solid #3E0E02'
